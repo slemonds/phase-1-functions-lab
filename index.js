2b0bb0b78472049a9a2c5a6f21b6dfb2e9c8c305
Soyoung Lemonds <soyoung.lemonds@yahoo.com>
2023-06-06 21:54:44 -0400

    Adds index.js codes

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    if (blocks <= 42) {
+        return 42 - blocks;
+    } else {
+        return blocks - 42;
+    }
+}
+
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264;
+} 
+
+function distanceTravelledInFeet(start, destination) {
+    if (distanceFromHqInFeet(start) > destination) {
+        return distanceFromHqInFeet(destination) - distanceFromHqInFeet(start);
+    } else {
+        return distanceFromHqInFeet(start) - distanceFromHqInFeet(destination);
+}
+    }
+    
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination) 
+    if (distance < 400 ) {
+        return 0;
+    } 
+    else if (distance > 400 && distance <= 2000) {
+        return (distance - 400) * 0.02; 
+    } 
+    else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } 
+    else if (distance > 2500) {
+        return 'cannot travel that far';
+    }
+}
